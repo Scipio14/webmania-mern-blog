6884aca6d42addbaf8e58d42a2fe33cb728cb843
Federico Macías <fma.abogado@gmail.com>
2023-03-07 18:53:13 -0600

    Added the FeaturedBlogs component

--- client/src/components/featuredBlogs/FeaturedBlogs.jsx
+++ client/src/components/featuredBlogs/FeaturedBlogs.jsx
@@ -1,5 +1,87 @@
-import classes from "./module.featuredBlogs.css";
+import classes from "./featuredBlogs.module.css";
+import mountainImg1 from "../../assets/mountain1.jpg";
+import mountainImg2 from "../../assets/mountain2.jpg";
+import mountainImg3 from "../../assets/mountain3.jpg";
+import { MdOutlinePreview } from "react-icons/md";
+import { AiFillLike } from "react-icons/ai";
+
 const FeaturedBlogs = () => {
-  return <div>FeaturedBlogs</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h3>Featured Blogs</h3>
+        <div className={classes.blogs}>
+          <div className={classes.left}>
+            <div className={classes.mainBlog}>
+              <img src={mountainImg1} />
+              <div className={classes.mainBlogData}>
+                <div className={classes.categoryAndMetadata}>
+                  <span className={classes.category}>Nature</span>
+                  <div className={classes.metadata}>
+                    <MdOutlinePreview /> 123 views
+                  </div>
+                  <div className={classes.metadata}>
+                    <AiFillLike /> 100 likes
+                  </div>
+                </div>
+                <h4>Blog 1 Title</h4>
+                <p className={classes.blogDesc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                  sint aliquam rem cupiditate ullam saepe.
+                </p>
+                <div className={classes.authorAndCreatedAt}>
+                  <span>
+                    <span>Author:</span> Scipio
+                  </span>
+                  <span>
+                    <span>Created:</span> October 31st 2022
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.right}>
+            <div className={classes.secondaryBlog}>
+              <img src={mountainImg2} />
+              <div className={classes.secondaryBlogData}>
+                <h4>Blog 2 title</h4>
+                <p className={classes.desc}>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+                  magni in dignissimos!
+                </p>
+                <div className={classes.authorAndCreatedAt}>
+                  <span>
+                    <span>Author:</span> Scipio
+                  </span>
+                  <br />
+                  <span>
+                    <span>Created:</span> October 30th 2022
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className={classes.secondaryBlog}>
+              <img src={mountainImg3} />
+              <div className={classes.secondaryBlogData}>
+                <h4>Blog 3 title</h4>
+                <p className={classes.desc}>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+                  magni in dignissimos!
+                </p>
+                <div className={classes.authorAndCreatedAt}>
+                  <span>
+                    <span>Author:</span> Scipio
+                  </span>
+                  <span>
+                    <span>Created:</span> October 30th 2022
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default FeaturedBlogs;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import classes from "./home.module.css";
-import { Navbar } from "../../components";
+import { Navbar,FeaturedBlogs } from "../../components";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <FeaturedBlogs/>
     </div>
   );
 };
